a539ef587db4bb410de6dda5319ee63eda4e7e4c
fauzandp <fauzan27dp@gmail.com>
2023-05-03 16:49:59 +0700

    Project10 | People App create UI and PersonCell class

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Fauzan Dwi Prasetyo on 03/05/23.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+ 
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+    
+}
